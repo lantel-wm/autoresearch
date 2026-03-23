25691c3e04071262d7d1e558614a9d7c3f756fb7
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-23 20:55:58 +0800

    Prune slower base features from core keeper

--- train.py
+++ train.py
@@ -12,24 +12,20 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][docs] open3d_top40_alpha158_core",
+        description="[factor][docs] open3d_top40_core_prune_slowbase",
         feature_expressions=[
             ("$close / $open - 1", "intraday_return"),
             ("$open / Ref($close, 1) - 1", "gap_return"),
             ("$close / Ref($close, 1) - 1", "close_return_1"),
             ("$close / Ref($close, 5) - 1", "close_return_5"),
             ("Mean($close, 5) / $close - 1", "ma_gap_5"),
-            ("Mean($close, 20) / $close - 1", "ma_gap_20"),
             ("Std($close, 5) / $close", "close_vol_5"),
-            ("Std($close, 20) / $close", "close_vol_20"),
             ("($high - $low) / $close", "range_pct"),
             ("Max($high, 5) / $close - 1", "high_breakout_5"),
             ("$close / Min($low, 5) - 1", "low_rebound_5"),
             ("Mean($volume, 5) / Mean($volume, 20) - 1", "volume_ratio_5_20"),
-            ("Std($volume, 20) / Mean($volume, 20)", "volume_vol_20"),
             ("$turnover_rate", "turnover_rate"),
             ("Mean($turnover_rate, 5)", "turnover_rate_mean_5"),
-            ("Std($turnover_rate, 20)", "turnover_rate_vol_20"),
             ("($close-$open)/($high-$low+1e-12)", "alpha_kmid2"),
             ("($high-Greater($open, $close))/($high-$low+1e-12)", "alpha_kup2"),
             ("(Less($open, $close)-$low)/($high-$low+1e-12)", "alpha_klow2"),
