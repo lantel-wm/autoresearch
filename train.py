f850f690ca5ad44ec413fd24725e6b0022a8d3bb
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-23 21:03:09 +0800

    Cap 3-day open label at 12 percent

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][docs] open3d_top40_alpha158_core",
+        description="[label][docs] open3d_capped12",
         feature_expressions=[
             ("$close / $open - 1", "intraday_return"),
             ("$open / Ref($close, 1) - 1", "gap_return"),
@@ -57,7 +57,9 @@ def build_experiment() -> ExperimentSpec:
                 "alpha_vsumd20",
             ),
         ],
-        label_expression="Ref($open, -4) / Ref($open, -1) - 1",
+        label_expression=(
+            "Greater(Less(Ref($open, -4) / Ref($open, -1) - 1, 0.12), -0.12)"
+        ),
         model_type="lgbm",
         model_kwargs={
             "n_estimators": 300,
