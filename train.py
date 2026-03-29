6a6d5761d7d3fd3af674981eec6ada7a0d771844
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 22:02:56 +0800

    factor: test cgo illiq55

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading negday_turnpressure5",
+        description="[factor][paper] behavioral_trading cgo_illiq55",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,6 +55,17 @@ def build_experiment() -> ExperimentSpec:
                 "Mean(Abs($close / $open - 1) * ($close < $open) * "
                 "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
                 "negday_turnpressure5",
+            ),
+            (
+                "((Sum(Ref($turnover_rate, 1) * Ref($close, 1), 55) / "
+                "(Sum(Ref($turnover_rate, 1), 55) + 1e-12)) / $close) - 1",
+                "cgo_anchor55",
+            ),
+            (
+                "((((Sum(Ref($turnover_rate, 1) * Ref($close, 1), 55) / "
+                "(Sum(Ref($turnover_rate, 1), 55) + 1e-12)) / $close) - 1) * "
+                "Mean(Abs($close / Ref($close, 1) - 1) / ($turnover_rate + 1e-12), 20))",
+                "cgo_illiq55",
             ),
             (
                 "($close / Ref($close, 10) - 1) * "
