bc3a1f231bb9687658e82514bf7a018fed23bf85
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 18:55:44 +0800

    Revert "factor: test neggap intraday reversal5"
    This reverts commit 90e21964d33e9bac47fae8515af979d62f75cd53.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] overnight_gap neggap_intraday_reversal5",
+        description="[factor][paper] behavioral_trading negday_turnpressure5",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -24,12 +24,6 @@ def build_experiment() -> ExperimentSpec:
             ("Std($open / Ref($close, 1) - 1, 20)", "gap_std20"),
             ("Rank($open / Ref($close, 1) - 1, 20)", "gap_rank20"),
             ("Mean((($close - $open) / $open) - ($open / Ref($close, 1) - 1), 5)", "gap_reversal5"),
-            (
-                "Mean((Abs($open / Ref($close, 1) - 1) + Abs($close / $open - 1)) * "
-                "($open < Ref($close, 1)) * ($close > $open) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
-                "neggap_intraday_reversal5",
-            ),
             ("$close / Ref($close, 5) - 1", "mom5"),
             ("$close / Ref($close, 10) - 1", "mom10"),
             (
