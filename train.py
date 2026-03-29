018b7d1bc3e986b7aa2e1bfe99141b23cfc0257d
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-30 04:00:29 +0800

    Restore kept train baseline

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] trend_alignment fip_poscgo55",
+        description="[factor][paper] trend_alignment fip_lowjump55",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -31,14 +31,6 @@ def build_experiment() -> ExperimentSpec:
                 "(Mean($close > Ref($close, 1), 55) - Mean($close < Ref($close, 1), 55))) / "
                 "(Std($close / Ref($close, 1) - 1, 55) + 1e-12))",
                 "fip_lowjump55",
-            ),
-            (
-                "((($close / Ref($close, 55) - 1) * "
-                "(Mean($close > Ref($close, 1), 55) - Mean($close < Ref($close, 1), 55))) / "
-                "(Std($close / Ref($close, 1) - 1, 55) + 1e-12)) * "
-                "Greater($close / ((Sum(Ref($turnover_rate, 1) * Ref($close, 1), 55) / "
-                "(Sum(Ref($turnover_rate, 1), 55) + 1e-12)) + 1e-12) - 1, 0)",
-                "fip_poscgo55",
             ),
             (
                 "($close / Ref($close, 5) - 1) * "
