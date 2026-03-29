2f25bb5ee970b692e97468b88732931688d092ac
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 18:29:21 +0800

    Revert "factor: test downside illiquidity pressure5"
    This reverts commit 8777266ee761e3dc649acb502f90c4e1016bb431.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] lowrisk_liquidity negday_illiqpressure5",
+        description="[factor][paper] behavioral_trading negday_turnpressure5",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,11 +55,6 @@ def build_experiment() -> ExperimentSpec:
                 "Mean(Abs($close / $open - 1) * ($close < $open) * "
                 "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
                 "negday_turnpressure5",
-            ),
-            (
-                "Mean(Abs($close / $open - 1) * ($close < $open) * "
-                "(Mean($turnover_rate, 20) / ($turnover_rate + 1e-12)), 5)",
-                "negday_illiqpressure5",
             ),
             (
                 "($close / Ref($close, 10) - 1) * "
