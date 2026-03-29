a8f9274dcb5f99bf8d8d7ce45aef3bf1748f4d3c
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 21:08:27 +0800

    Revert "factor: test salience turnover20"
    This reverts commit 34f8227509f150110f49ce5ed1c456a6b87cbf84.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading salience_turn20",
+        description="[factor][paper] behavioral_trading negday_turnpressure5",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,21 +55,6 @@ def build_experiment() -> ExperimentSpec:
                 "Mean(Abs($close / $open - 1) * ($close < $open) * "
                 "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
                 "negday_turnpressure5",
-            ),
-            (
-                "Min((($close / Ref($close, 1) - 1) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12))), 20)",
-                "daily_loser_salience20",
-            ),
-            (
-                "Max((($close / Ref($close, 1) - 1) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12))), 20)",
-                "daily_winner_salience20",
-            ),
-            (
-                "Max(Abs($close / Ref($close, 1) - 1) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 20)",
-                "daily_abs_salience20",
             ),
             (
                 "($close / Ref($close, 10) - 1) * "
