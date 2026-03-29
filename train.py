0712d754b92cb8e4e47785fa0ddac0f4631df289
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-30 06:09:16 +0800

    factor: test overnight lowrisk20

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] trend_alignment fip_lowjump55",
+        description="[factor][paper] overnight_gap overnight_lowrisk20",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -24,6 +24,12 @@ def build_experiment() -> ExperimentSpec:
             ("Std($open / Ref($close, 1) - 1, 20)", "gap_std20"),
             ("Rank($open / Ref($close, 1) - 1, 20)", "gap_rank20"),
             ("Mean((($close - $open) / $open) - ($open / Ref($close, 1) - 1), 5)", "gap_reversal5"),
+            (
+                "(Sum($open / Ref($close, 1) - 1, 20) / "
+                "(Sum(Abs($open / Ref($close, 1) - 1), 20) + 1e-12)) * "
+                "(Mean($turnover_rate, 20) / (Std($turnover_rate, 20) + 1e-12))",
+                "overnight_lowrisk20",
+            ),
             ("$close / Ref($close, 5) - 1", "mom5"),
             ("$close / Ref($close, 10) - 1", "mom10"),
             (
