da55862f24852942619d9f4f9a67fe965f2adb28
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 18:05:14 +0800

    Revert "factor: test negpos turnover imbalance5"
    This reverts commit 24b357b51f7cfdf03e469a14c33e86ac306d9779.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading negpos_turnimbalance5",
+        description="[factor][paper] behavioral_trading negday_turnpressure5",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -52,16 +52,9 @@ def build_experiment() -> ExperimentSpec:
                 "posday_turnshock1",
             ),
             (
-                "("
                 "Mean(Abs($close / $open - 1) * ($close < $open) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5) - "
-                "Mean(Abs($close / $open - 1) * ($close > $open) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)"
-                ") / ("
-                "Mean(Abs($close / $open - 1) * "
-                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5) + 1e-12"
-                ")",
-                "negpos_turnimbalance5",
+                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
+                "negday_turnpressure5",
             ),
             (
                 "($close / Ref($close, 10) - 1) * "
