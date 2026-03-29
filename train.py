118ead88042884aa4ebeba084976d283276e4fbe
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-29 18:38:22 +0800

    factor: test downside turnover pressure stable5

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading negday_turnpressure5",
+        description="[factor][paper] lowrisk_liquidity negday_turnpressure_stable5",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,6 +55,12 @@ def build_experiment() -> ExperimentSpec:
                 "Mean(Abs($close / $open - 1) * ($close < $open) * "
                 "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5)",
                 "negday_turnpressure5",
+            ),
+            (
+                "Mean(Abs($close / $open - 1) * ($close < $open) * "
+                "($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)), 5) * "
+                "(Mean($turnover_rate, 20) / (Std($turnover_rate, 20) + 1e-12))",
+                "negday_turnpressure_stable5",
             ),
             (
                 "($close / Ref($close, 10) - 1) * "
