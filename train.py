04d6ae5a6e386eb20c65dfe2360a48232c70e9fc
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-30 12:55:52 +0800

    factor: test stmom turnregime20_120

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading dailymom_turnstable20",
+        description="[factor][paper] behavioral_trading stmom_turnregime20_120",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -38,6 +38,12 @@ def build_experiment() -> ExperimentSpec:
                 "(($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)) / "
                 "((Std($turnover_rate, 20) / (Mean($turnover_rate, 20) + 1e-12)) + 1e-12)), 20)",
                 "dailymom_turnstable20",
+            ),
+            (
+                "($close / Ref($close, 20) - 1) * "
+                "((Mean($turnover_rate, 20) > Mean($turnover_rate, 120)) - "
+                "(Mean($turnover_rate, 20) < Mean($turnover_rate, 120)))",
+                "stmom_turnregime20_120",
             ),
             (
                 "((($close / Ref($close, 55) - 1) * "
