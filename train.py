8daf7fb84602264a48c850a273160c23fe6d2c2b
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-30 09:21:46 +0800

    factor: test lowturn stable20_60

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading dailymom_turnstable20",
+        description="[factor][paper] lowrisk_liquidity lowturn_stable20_60",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -38,6 +38,11 @@ def build_experiment() -> ExperimentSpec:
                 "(($turnover_rate / (Mean($turnover_rate, 20) + 1e-12)) / "
                 "((Std($turnover_rate, 20) / (Mean($turnover_rate, 20) + 1e-12)) + 1e-12)), 20)",
                 "dailymom_turnstable20",
+            ),
+            (
+                "(Mean($turnover_rate, 60) - Mean($turnover_rate, 20)) / "
+                "(Std($turnover_rate, 20) + 1e-12)",
+                "lowturn_stable20_60",
             ),
             (
                 "((($close / Ref($close, 55) - 1) * "
