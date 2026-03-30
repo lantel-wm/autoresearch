eb8fad9b545efb904cd82c13171fb87bb496c22b
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-30 22:00:53 +0800

    factor: test losspath illiqshock20_120

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading mindrop_losscrowd20_120",
+        description="[factor][paper] behavioral_trading losspath_illiqshock20_120",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,6 +55,13 @@ def build_experiment() -> ExperimentSpec:
                 "(Mean($turnover_rate, 20) / (Mean($turnover_rate, 120) + 1e-12)) * "
                 "Greater(0 - ($close / Ref($close, 20) - 1), 0)",
                 "mindrop_losscrowd20_120",
+            ),
+            (
+                "Greater(0 - ($close / Ref($close, 20) - 1), 0) * "
+                "Greater(Mean($close < Ref($close, 1), 20) - Mean($close > Ref($close, 1), 20), 0) * "
+                "((Mean(Abs($close / Ref($close, 1) - 1) / ($turnover_rate + 1e-12), 20)) / "
+                "(Mean(Abs($close / Ref($close, 1) - 1) / ($turnover_rate + 1e-12), 120) + 1e-12))",
+                "losspath_illiqshock20_120",
             ),
             (
                 "((($close / Ref($close, 55) - 1) * "
