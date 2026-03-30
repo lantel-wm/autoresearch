90fa315f6088c21b661caa0642cbd0d0c7a62ab6
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-31 00:27:28 +0800

    factor: test negsalience turn20_120

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading mindrop_losscrowd20_120",
+        description="[factor][paper] behavioral_trading negsalience_turn20_120",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,6 +55,11 @@ def build_experiment() -> ExperimentSpec:
                 "(Mean($turnover_rate, 20) / (Mean($turnover_rate, 120) + 1e-12)) * "
                 "Greater(0 - ($close / Ref($close, 20) - 1), 0)",
                 "mindrop_losscrowd20_120",
+            ),
+            (
+                "(0 - Min((($close / Ref($close, 1) - 1) * "
+                "($turnover_rate / (Mean($turnover_rate, 120) + 1e-12))), 20))",
+                "negsalience_turn20_120",
             ),
             (
                 "((($close / Ref($close, 55) - 1) * "
