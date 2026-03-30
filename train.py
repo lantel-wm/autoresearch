da02d545c9d9c40217ef215b2342141850b41499
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2026-03-31 03:02:11 +0800

    Restore kept train baseline

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from prepare import ExperimentSpec, run_experiment
 
 def build_experiment() -> ExperimentSpec:
     return ExperimentSpec(
-        description="[factor][paper] behavioral_trading mindrop_cgogain20_55",
+        description="[factor][paper] behavioral_trading mindrop_losscrowd20_120",
         feature_expressions=[
             ("($close - $open) / $open", "kmid"),
             ("($high - $low) / $open", "klen"),
@@ -55,12 +55,6 @@ def build_experiment() -> ExperimentSpec:
                 "(Mean($turnover_rate, 20) / (Mean($turnover_rate, 120) + 1e-12)) * "
                 "Greater(0 - ($close / Ref($close, 20) - 1), 0)",
                 "mindrop_losscrowd20_120",
-            ),
-            (
-                "((0 - Min($close / Ref($close, 1) - 1, 20))) * "
-                "Greater(0 - (((Sum(Ref($turnover_rate, 1) * Ref($close, 1), 55) / "
-                "(Sum(Ref($turnover_rate, 1), 55) + 1e-12)) / $close) - 1), 0)",
-                "mindrop_cgogain20_55",
             ),
             (
                 "((($close / Ref($close, 55) - 1) * "
